84166e747e0c634ee098c8eab81b9d97511862d8
utkarsh-009 <utkarshpatil321@gmail.com>
2022-05-14 01:25:36 +0530

    splash screen added

---
+++ netflix_app/lib/Screens/homeScreen.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+class HomeScreen extends StatefulWidget {
+  const HomeScreen({Key? key}) : super(key: key);
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Color(0xFF121212),
+      appBar: AppBar(
+        backgroundColor: Color(0xFF121212),
+        centerTitle: true,
+        title: Text(
+          "Netflix",
+          style: GoogleFonts.bebasNeue(fontSize: 25, color: Colors.red),
+        ),
+        leading: IconButton(
+          onPressed: () {},
+          icon: Icon(Icons.menu),
+        ),
+        actions: [
+          IconButton(
+            onPressed: () {},
+            icon: Icon(Icons.search),
+          )
+        ],
+      ),
+    );
+  }
+}
--- netflix_app/lib/main.dart
+++ netflix_app/lib/main.dart
@@ -1,5 +1,7 @@
+import 'package:animated_splash_screen/animated_splash_screen.dart';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'package:netflix_app/Screens/homeScreen.dart';
 
 void main() {
   runApp(const MyApp());
@@ -12,40 +14,14 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: NetflixApp(),
-    );
-  }
-}
-
-class NetflixApp extends StatefulWidget {
-  const NetflixApp({Key? key}) : super(key: key);
-
-  @override
-  State<NetflixApp> createState() => _NetflixAppState();
-}
-
-class _NetflixAppState extends State<NetflixApp> {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: Color(0xFF121212),
-      appBar: AppBar(
+      home: AnimatedSplashScreen(
+        splash: Image.asset(
+          'assets/images/Netflix.png',
+        ),
+        splashIconSize: double.infinity,
+        splashTransition: SplashTransition.fadeTransition,
+        nextScreen: HomeScreen(),
         backgroundColor: Color(0xFF121212),
-        centerTitle: true,
-        title: Text(
-          "Netflix",
-          style: GoogleFonts.bebasNeue(fontSize: 25, color: Colors.red),
-        ),
-        leading: IconButton(
-          onPressed: () {},
-          icon: Icon(Icons.menu),
-        ),
-        actions: [
-          IconButton(
-            onPressed: () {},
-            icon: Icon(Icons.search),
-          )
-        ],
       ),
     );
   }
