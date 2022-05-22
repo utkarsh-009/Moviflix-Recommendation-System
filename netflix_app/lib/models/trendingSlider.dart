83e5e1bc4098bda54b7898055f0221a0247857b4
utkarsh-009 <utkarshpatil321@gmail.com>
2022-05-23 01:55:45 +0530

    initialised api key and read access token

--- netflix_app/lib/models/trendingSlider.dart
+++ netflix_app/lib/models/trendingSlider.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:tmdb_api/tmdb_api.dart';
 
 class TrendingSlider extends StatefulWidget {
   const TrendingSlider({Key? key}) : super(key: key);
@@ -14,8 +15,11 @@ class _TrendingSliderState extends State<TrendingSlider> {
   final readaccesstoken =
       'eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIxZTZlODRiMmVlOTc1YmIwMDRiMTY0MzgyNzE1Y2Y3OCIsInN1YiI6IjYyOGE0ODY0MjQ5NWFiNTI3ZGQxMDM3YSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.h7nA0i6DrGUcT96vmODGtk_Qh2r4u15kvsXqmhh_kPI';
 
-  loadMovies() async{
-    
+  loadMovies() async {
+    TMDB tmdbWithCustomLogs = TMDB(
+      ApiKeys(apikey, readaccesstoken),
+      logConfig: ConfigLogger(showLogs: true, showErrorLogs: true),
+    );
   }
 
   Widget build(BuildContext context) {
