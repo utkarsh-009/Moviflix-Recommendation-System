4b80ac5f006d30ff2a71200c81f0827d9fc24418
utkarsh-009 <utkarshpatil321@gmail.com>
2022-05-28 00:36:29 +0530

    drawer ui done

--- moviflix_app/lib/widgets/myDrawerList.dart
+++ moviflix_app/lib/widgets/myDrawerList.dart
@@ -9,83 +9,87 @@ class MyDrawerList extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      height: 500,
-      color: Color(0xFF121212),
-      child: ListView(
-        children: [
-          ListTile(
-            leading: Icon(
-              Icons.trending_up_rounded,
-              size: 30,
-              color: Colors.white,
-            ),
-            title: Text(
-              "Trending Movies",
-              style: GoogleFonts.bebasNeue(
-                  fontSize: 23,
+    return Column(
+      children: [
+        Container(
+          height: 600,
+          color: Color(0xFF121212),
+          child: ListView(
+            children: [
+              ListTile(
+                leading: Icon(
+                  Icons.trending_up_rounded,
+                  size: 30,
                   color: Colors.white,
-                  fontWeight: FontWeight.w100),
-            ),
-            onTap: () {},
-          ),
-          ListTile(
-            leading: Icon(
-              Icons.star_border,
-              size: 30,
-              color: Colors.white,
-            ),
-            title: Text(
-              "Top Rated Movies",
-              style: GoogleFonts.bebasNeue(
-                  fontSize: 23,
-                  color: Colors.white,
-                  fontWeight: FontWeight.w100),
-            ),
-            onTap: () {},
-          ),
-          ListTile(
-            leading: Icon(
-              Icons.recommend,
-              size: 30,
-              color: Colors.white,
-            ),
-            title: Text(
-              "Recommended Movies",
-              style: GoogleFonts.bebasNeue(
-                  fontSize: 23,
-                  color: Colors.white,
-                  fontWeight: FontWeight.w100),
-            ),
-            onTap: () {},
-          ),
-          Divider(
-            color: Colors.white,
-          ),
-          ListTile(
-            leading: Icon(
-              Icons.home,
-              size: 30,
-              color: Colors.white,
-            ),
-            title: Text(
-              "Home",
-              style: GoogleFonts.bebasNeue(
-                  fontSize: 23,
-                  color: Colors.white,
-                  fontWeight: FontWeight.w100),
-            ),
-            onTap: () {
-              Navigator.push(
-                context,
-                MaterialPageRoute(
-                  builder: (context) => HomeScreen(),
                 ),
-              );
-            },
+                title: Text(
+                  "Trending Movies",
+                  style: GoogleFonts.bebasNeue(
+                      fontSize: 23,
+                      color: Colors.white,
+                      fontWeight: FontWeight.w100),
+                ),
+                onTap: () {},
+              ),
+              ListTile(
+                leading: Icon(
+                  Icons.star_border,
+                  size: 30,
+                  color: Colors.white,
+                ),
+                title: Text(
+                  "Top Rated Movies",
+                  style: GoogleFonts.bebasNeue(
+                      fontSize: 23,
+                      color: Colors.white,
+                      fontWeight: FontWeight.w100),
+                ),
+                onTap: () {},
+              ),
+              ListTile(
+                leading: Icon(
+                  Icons.recommend,
+                  size: 30,
+                  color: Colors.white,
+                ),
+                title: Text(
+                  "Recommended Movies",
+                  style: GoogleFonts.bebasNeue(
+                      fontSize: 23,
+                      color: Colors.white,
+                      fontWeight: FontWeight.w100),
+                ),
+                onTap: () {},
+              ),
+              Divider(
+                color: Colors.white,
+              ),
+              ListTile(
+                leading: Icon(
+                  Icons.home,
+                  size: 30,
+                  color: Colors.white,
+                ),
+                title: Text(
+                  "Home",
+                  style: GoogleFonts.bebasNeue(
+                      fontSize: 23,
+                      color: Colors.white,
+                      fontWeight: FontWeight.w100),
+                ),
+                onTap: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                      builder: (context) => HomeScreen(),
+                    ),
+                  );
+                },
+              ),
+            ],
           ),
-        ],
-      ),
+        ),
+      ],
     );
   }
 }
